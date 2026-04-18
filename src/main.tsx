e1a18912812e85fb4722d5b0913d1fb34081e701
RoomiestBetse <betseessilfie@gmail.com>
2026-04-18 02:29:14 -0600

    Scroll to top on page reload

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Always start at the top on reload
+if ("scrollRestoration" in history) history.scrollRestoration = "manual";
+window.scrollTo(0, 0);
+
 createRoot(document.getElementById("root")!).render(<App />);
